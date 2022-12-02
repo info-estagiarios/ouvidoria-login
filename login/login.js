50eb8f343eede59470121554bf2af23ce26aed0b
info-estagiarios <114098437+info-estagiarios@users.noreply.github.com>
2022-12-02 11:12:49 -0300

    Update login.js

--- login/login.js
+++ login/login.js
@@ -48,3 +48,22 @@ loginUsuario.addEventListener('click', function(){
     login.style.display = "flex"
     cadastrar.style.display = "none"
 })
+
+function senha(){
+    var s1 = document.querySelector("#pass1")
+    var s2 = document.querySelector("#pass2")
+    var mensagem = document.querySelector(".mensagem")
+    a = String(s1.value)
+    b = String(s2.value)
+
+    if(a == b && s1.value.length > 7){
+        s1.style.border = "solid 1px rgb(26, 255, 0)"
+        s2.style.border = "solid 1px rgb(26, 255, 0)"
+        mensagem.style.display = "none"
+    }else{
+        s1.style.border = "solid 1px red"
+        s2.style.border = "solid 1px red"
+        mensagem.style.display = "block"
+        mensagem.style.color = "red"
+    }
+}
